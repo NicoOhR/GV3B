e0eaa060f82a7fa8a098c11b6987c77faa3130ea
NicoOhR <nimoohayonr@gmail.com>
2024-10-20 15:51:46 -0500

    commiting to bevy_rapier2d

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,38 @@
-use bevy::{ecs::query, prelude::*};
-
-#[derive(Component)]
-struct Body;
-
-#[derive(Resource)]
-struct Velocity(f32);
-
-fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
-
-    commands.spawn((
-        SpriteBundle {
-            sprite: Sprite {
-                color: Color::srgb(1.0, 0.0, 0.0),
-                custom_size: Some(Vec2::new(50.0, 50.0)),
-                ..default()
-            },
-            ..default()
-        },
-        Body,
-    ));
-}
-
-fn move_ball(time: Res<Time>, mut query: Query<(&Body, &mut Transform)>, speed: Res<Velocity>) {
-    for (_, mut transform) in &mut query {
-        transform.translation.x += speed.0 * time.delta_seconds();
-
-        if transform.translation.x > 500.0 {
-            transform.translation.x = -500.0;
-        }
-    }
-}
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(Velocity(40.0))
-        .add_systems(Startup, setup)
-        .add_systems(Update, move_ball)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        .add_plugins(RapierDebugRenderPlugin::default())
+        .add_systems(Startup, setup_graphics)
+        .add_systems(Startup, setup_physics)
+        .add_systems(Update, print_ball_altitude)
         .run();
+}
+
+fn setup_graphics(mut commands: Commands) {
+    // Add a camera so we can see the debug-render.
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn setup_physics(mut commands: Commands) {
+    /* Create the ground. */
+    commands
+        .spawn(Collider::cuboid(500.0, 50.0))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)));
+
+    /* Create the bouncing ball. */
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Collider::ball(50.0))
+        .insert(Restitution::coefficient(0.7))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)));
+}
+
+fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
+    for transform in positions.iter() {
+        println!("Ball altitude: {}", transform.translation.y);
+    }
 }
