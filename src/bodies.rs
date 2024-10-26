00fb0957a363afce40069a58c566f7393dad3e65
NicoOhR <nimoohayonr@gmail.com>
2024-10-25 22:25:09 -0500

    switched to actual mass + actual orbit

--- src/bodies.rs
+++ src/bodies.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 use bevy_prototype_lyon::prelude::*;
 use bevy_rapier2d::prelude::*;
+use physical_constants::{self, NEWTONIAN_CONSTANT_OF_GRAVITATION};
 use rapier2d::na::Vector2;
 
 #[derive(Component)]
@@ -14,18 +15,26 @@ pub fn gravitational_force(
 ) -> Vector2<f32> {
     let r = position2 - position1;
     let direction = r.norm();
-    let f_mag = 1000000.0 * ((mass1 * mass2) / direction.powi(2));
+    let f_mag = 1000.0 * ((mass1 * mass2) / direction.powi(2));
     r.normalize() * f_mag
 }
 
-pub fn apply_gravity(mut bodies: Query<(&Collider, &Transform, &mut ExternalForce)>) {
+pub fn apply_gravity(mut bodies: Query<(&ColliderMassProperties, &Transform, &mut ExternalForce)>) {
     let mut combinations = bodies.iter_combinations_mut::<2>();
     while let Some([body1, body2]) = combinations.fetch_next() {
-        let (mass1, translation1, mut ex_force) = body1;
-        let (mass2, translation2, _) = body2;
+        let (mass_properties_1, translation1, mut ex_force) = body1;
+        let (mass_properties_2, translation2, _) = body2;
+        let mass1 = match mass_properties_1 {
+            ColliderMassProperties::Mass(mass) => Some(*mass),
+            _ => Some(0.0),
+        };
+        let mass2 = match mass_properties_2 {
+            ColliderMassProperties::Mass(mass) => Some(*mass),
+            _ => Some(0.0),
+        };
         let f_1_2 = gravitational_force(
-            mass1.as_ball().unwrap().radius(),
-            mass2.as_ball().unwrap().radius(),
+            mass1.unwrap(),
+            mass2.unwrap(),
             translation1.translation.truncate().into(),
             translation2.translation.truncate().into(),
         );
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use bevy::prelude::*;
+use bevy::render::camera::ScalingMode;
 use bevy_prototype_lyon::prelude::*;
-use bevy_rapier2d::prelude::*;
-use physical_constants::{self, NEWTONIAN_CONSTANT_OF_GRAVITATION};
+use bevy_rapier2d::{prelude::*, rapier::prelude::MassProperties};
 use rapier2d::na::Vector2;
-
 mod bodies;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(10.0))
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(20.0))
         .add_plugins(RapierDebugRenderPlugin::default())
         .add_plugins(ShapePlugin)
         .add_systems(Startup, setup_graphics)
@@ -25,8 +24,8 @@ fn setup_graphics(mut commands: Commands) {
 
 fn setup_physics(mut commands: Commands) {
     /* Create the ground. */
-    let mass1 = 40.0;
-    let mass2 = 200.0;
+    let mass1 = 60.0;
+    let mass2 = 600.0;
 
     let line = shapes::Line(Vec2::ZERO, Vec2::new(100.0, 0.0));
     commands.spawn((
@@ -40,9 +39,10 @@ fn setup_physics(mut commands: Commands) {
 
     commands
         .spawn(RigidBody::Dynamic)
-        .insert(Collider::ball(mass1))
+        .insert(Collider::ball(40.0))
         .insert(Restitution::coefficient(0.7))
         .insert(GravityScale(0.0))
+        .insert(ColliderMassProperties::Mass(mass1))
         .insert(ExternalForce::default())
         .insert(Velocity {
             linvel: Vec2::new(0.0, 25.0),
@@ -52,8 +52,9 @@ fn setup_physics(mut commands: Commands) {
 
     commands
         .spawn(RigidBody::Dynamic)
-        .insert(Collider::ball(mass2))
+        .insert(Collider::ball(40.0))
         .insert(Restitution::coefficient(0.9))
+        .insert(ColliderMassProperties::Mass(mass2))
         .insert(GravityScale(0.0))
         .insert(ExternalForce::default())
         .insert(TransformBundle::from(Transform::from_xyz(-50.0, 0.0, 0.0)));
