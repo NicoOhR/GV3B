518d9b7b3fa86a6bdd264fac22ea612855ba332c
NicoOhR <nimoohayonr@gmail.com>
2024-10-27 19:13:26 -0500

    lazy solution to follow biggest instead

--- src/main.rs
+++ src/main.rs
@@ -40,25 +40,34 @@ fn setup_graphics(mut commands: Commands) {
 }
 
 fn camera_update(
-    q_bodies: Query<&Transform, With<Velocity>>,
+    q_bodies: Query<(&Transform, &ColliderMassProperties), With<Velocity>>,
     mut camera: Query<&mut Transform, (With<Camera2d>, Without<Velocity>)>,
 ) {
-    let mut average: Vec3 = Vec3::ZERO;
-    for transform in q_bodies.iter() {
-        average = average + transform.translation;
+    let mut biggest_pos: Vec3 = Vec3::ZERO;
+    let mut biggest_mass: f32 = 0.0;
+
+    for (transform, mass) in q_bodies.iter() {
+        let current = match mass {
+            ColliderMassProperties::Mass(some_mass) => Some(*some_mass),
+            _ => Some(0.0),
+        };
+
+        if current.unwrap() > biggest_mass {
+            biggest_pos = transform.translation;
+            biggest_mass = current.unwrap()
+        }
     }
-    average /= 3.0;
     for mut transform in camera.iter_mut() {
-        average.z = transform.translation.z;
-        transform.translation = average;
+        biggest_pos.z = transform.translation.z;
+        transform.translation = biggest_pos;
     }
 }
 
 fn setup_physics(mut commands: Commands) {
     /* Create the ground. */
-    let mass1 = 60.0;
-    let mass2 = 60.0;
-    let mass3 = 60.0;
+    let mass1 = 300.0;
+    let mass2 = 600.0;
+    let mass3 = 300.0;
 
     commands
         .spawn(RigidBody::Dynamic)
@@ -78,7 +87,7 @@ fn setup_physics(mut commands: Commands) {
         .insert(ColliderMassProperties::Mass(mass3))
         .insert(ExternalForce::default())
         .insert(Velocity {
-            linvel: Vec2::new(0.0, 25.0),
+            linvel: Vec2::new(0.0, 0.0),
             ..default()
         })
         .insert(TransformBundle::from(Transform::from_xyz(-600.0, 0.0, 0.0)));
